0ca7ddfb44c8022031bc3a67c43b3f0e329b3386
Gábor Gulyás <122649612+gabor-gulyas@users.noreply.github.com>
2024-02-28 10:17:16 +0100

    Update softclipremover.py

--- softclipremover.py
+++ softclipremover.py
@@ -1,31 +1,31 @@
 import re
-### pandas telepítés ellenőrzése
+### pandas installation verification
 
-print("pandas modul ellenőrzése...")
+print("pandas module check...")
 try:
     import pandas as pd
     print("OK!")
 except ImportError:
-    print("Pandas nincs telepítve. Telepítés...")
+    print("Pandas are not installed. Installation...")
     try:
         # Pandas telepítése
         import subprocess
 
         subprocess.check_call(["pip", "install", "pandas"])
-        print("Pandas sikeresen telepítve.")
+        print("Pandas successfully installed.")
 
-        # Újraimportálás a frissen telepített Pandas használatához
+        # Re-import to use freshly installed Pandas
         import pandas as pd
     except Exception as e:
-        print(f"Hiba a Pandas telepítése során: {e}")
+        print(f"Error during the installation of Pandas: {e}")
         exit()
 
-# A sam file elérési útja
+# input sam file path
 file_path = "/mnt/e/armin/MPOX_KSHV_cage_A_20230131_GCGG.Aligned.out.sam"
-# A kimeneti sam file elérési útja .sam a vége!!!
+# output path. if u would like sam file, dont forget the sam extension
 out_path = "/mnt/"
 
-def process_file(file_path):
+def softclipremover(file_path):
     sam_data = pd.DataFrame()
     try:
         with open(file_path, 'r') as file:
@@ -42,7 +42,7 @@ def process_file(file_path):
                     QUALITY = columns[10] if len(columns) > 5 else ''
                     num_of_s = CIGAR.count('S')
 
-                    # amikor mindkét vége softclip
+                    # Softclip are in both end
                     if num_of_s == 2:
                         start_s_index = CIGAR.find('S')
                         end_s_index = CIGAR.rfind('S')
@@ -61,7 +61,7 @@ def process_file(file_path):
                         start_s_index = CIGAR.find('S')
                         start_m_index = CIGAR.rfind('M')
 
-                        # amikor a softclip a jobb végén van
+                        # Soft clip is right end
                         if start_s_index > start_m_index:
                             rcutvalue = int(re.split('[M]', CIGAR[start_m_index + 1:start_s_index])[0])
                             lcutvalue = 0
@@ -72,7 +72,7 @@ def process_file(file_path):
                             columns[10] = corrected_quality
                             sam_data = pd.concat([sam_data, pd.DataFrame([columns])], ignore_index=True)
 
-                        # amikor a softclip a bal végén van
+                        # Softclip in left end
                         else:
                             rcutvalue = 0
                             lcutvalue = int(CIGAR[:start_s_index].strip())
@@ -84,16 +84,16 @@ def process_file(file_path):
                             sam_data = pd.concat([sam_data, pd.DataFrame([columns])], ignore_index=True)
 
 
-                    # amikor a read unmapped ezért nem változtatunk az adaton
+                    # When the reads are unmapped we dont change the data
                     elif num_of_s == 0:
                         sam_data = pd.concat([sam_data, pd.DataFrame([columns])], ignore_index=True)
     except FileNotFoundError:
-        print(f"A fájl nem található: {file_path}")
+        print(f"The file is not found {file_path}")
     except Exception as e:
-        print(f"Hiba történt: {e}")
-    print(sam_data)
+        print(f"Error: {e}")
+    return sam_data
 
-# Hívja meg a függvényt a fájl feldolgozásához
-output = process_file(file_path)
+# process
+output = softclipremover(file_path)
 
 output.to_csv(out_path, sep='\t', index=False, header=False)
